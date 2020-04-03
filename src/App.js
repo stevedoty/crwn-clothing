d48583c42f6cfcf47627d201e54efc01798dc9bd
Steve Doty <stevedoty16@gmail.com>
2020-04-03 11:24:54 -0700

    create Header component and styles; create links within header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+
 import './App.css';
 
+import Header from './components/header/header.component';
 import HomePage from './components/pages/homepage/homepage.component';
 import ShopPage from './components/pages/shop/shop.component';
 
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route  path='/shop' component={ShopPage} />

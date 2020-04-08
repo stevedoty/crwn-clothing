c8de4ad913401929a13506a27d83291e34f7297d
Steve Doty <stevedoty16@gmail.com>
2020-04-07 21:15:26 -0700

    create CartIcon and CartDropdown

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -3,8 +3,10 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
@@ -20,15 +22,18 @@ const Header = ({ currentUser }) => (
             <Link className='option' to='/shop'>
                 CONTACT
             </Link>
-            { currentUser ?
+            { currentUser ? (
                 <div className='option' onClick={() => auth.signOut()} >
                     SIGN OUT
                 </div>
-                : <Link className='option' to='/signin'>
+               ) : (
+                <Link className='option' to='/signin'>
                     SIGN IN
                 </Link>
-            }
+            )}
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 )
 

9d63a105edcfd9429e6a9f0cee510b92d7d6d164
Steve Doty <stevedoty16@gmail.com>
2020-08-04 16:39:27 -0700

    Updates

---
+++ precache-manifest.0caf1509a1ff9402d4433c50bdc6dba6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03db35ba70b82694d72e67d8f4e36008",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "5e71623e666ed53dee6f",
+    "url": "/crwn-clothing/static/css/main.a79aada8.chunk.css"
+  },
+  {
+    "revision": "17faf09d98d2dce93563",
+    "url": "/crwn-clothing/static/js/2.f7b1d934.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/crwn-clothing/static/js/2.f7b1d934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e71623e666ed53dee6f",
+    "url": "/crwn-clothing/static/js/main.f8ae6895.chunk.js"
+  },
+  {
+    "revision": "0c0bf0a54c9e5596672f",
+    "url": "/crwn-clothing/static/js/runtime-main.5ffcf0f1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
